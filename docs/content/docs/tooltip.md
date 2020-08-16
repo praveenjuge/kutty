dd1662dbbb90e49d7410c9955ec3de0fb647f25f
praveenjuge <praveen@skcript.com>
2020-08-16 16:09:32 +0530

    Tooltip Docs
    Signed-off-by: praveenjuge <praveen@skcript.com>

--- docs/content/docs/tooltip.md
+++ docs/content/docs/tooltip.md
@@ -1,44 +1,30 @@
 ---
-title: Tooltips
+title: Tooltip
 ---
 
 ## Basic
 
 {{< code html >}}
-<button class="btn btn-light" x-data="tooltip()" x-spread="tooltip" title="Tooltip">
-  Tooltip
-</button>
+<button class="btn btn-light" x-data="tooltip()" x-spread="tooltip" title="Regular Tooltip">Tooltip</button>
 {{< /code >}}
 
-
-## Placement - Top
+## Placement
 
 {{< code html >}}
-<button class="btn btn-light" x-data="tooltip()" x-spread="tooltip" x-position="top" title="Tooltip on top">
-  Tooltip on top
-</button>
+<button class="btn btn-light" x-data="tooltip()" x-spread="tooltip" x-position="top" title="Tooltip on top">Tooltip on top</button>
+<button class="btn btn-light" x-data="tooltip()" x-spread="tooltip" x-position="left" title="Tooltip on left">Tooltip on left</button>
+<button class="btn btn-light" x-data="tooltip()" x-spread="tooltip" x-position="bottom" title="Tooltip on bottom">Tooltip on bottom</button>
+<button class="btn btn-light" x-data="tooltip()" x-spread="tooltip" x-position="right" title="Tooltip on right">Tooltip on right</button>
 {{< /code >}}
 
-## Placement - Left
+## On Card
 
 {{< code html >}}
-<button class="btn btn-light" x-data="tooltip()" x-spread="tooltip" x-position="left" title="Tooltip on left">
-  Tooltip on left
-</button>
-{{< /code >}}
 
-## Placement - Bottom
-
-{{< code html >}}
-<button class="btn btn-light" x-data="tooltip()" x-spread="tooltip" x-position="bottom" title="Tooltip on bottom">
-  Tooltip on bottom
-</button>
-{{< /code >}}
-
-## Placement - Right
-
-{{< code html >}}
-<button class="btn btn-light" x-data="tooltip()" x-spread="tooltip" x-position="right" title="Tooltip on right">
-  Tooltip on right
-</button>
+<a href="#" class="card" x-data="tooltip()" x-spread="tooltip" title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.">
+  <div class="card-header">Normal Card</div>
+  <div class="card-body">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p>
+  </div>
+</a>
 {{< /code >}}
--- src/js/tooltip.js
+++ src/js/tooltip.js
@@ -1,61 +1,69 @@
-const getTooltipPlacement = function(element, tooltip) {
-  let placement = element.getAttribute('x-position') || 'top';
+const getTooltipPlacement = function (element, tooltip) {
+  let placement = element.getAttribute("x-position") || "top";
   let transform = {};
-  switch(placement) {
-    case 'top': {
-      transform = { 
-        x: tooltip.offsetWidth > element.offsetWidth ? Math.round(element.offsetLeft - (Math.abs(element.offsetWidth - tooltip.offsetWidth)/2)) : Math.round(element.offsetLeft + (Math.abs(element.offsetWidth - tooltip.offsetWidth)/2)), 
-        y: Math.round(element.offsetTop - tooltip.offsetHeight - 10)
+  switch (placement) {
+    case "top": {
+      transform = {
+        x:
+          tooltip.offsetWidth > element.offsetWidth
+            ? Math.round(element.offsetLeft - Math.abs(element.offsetWidth - tooltip.offsetWidth) / 2)
+            : Math.round(element.offsetLeft + Math.abs(element.offsetWidth - tooltip.offsetWidth) / 2),
+        y: Math.round(element.offsetTop - tooltip.offsetHeight - 10),
       };
       break;
     }
-    case 'left': {
-      transform = { 
-        x: Math.round(element.offsetLeft - tooltip.offsetWidth - 10), 
-        y: Math.round(element.offsetTop + (element.offsetHeight / 2) - (tooltip.offsetHeight / 2))
+    case "left": {
+      transform = {
+        x: Math.round(element.offsetLeft - tooltip.offsetWidth - 10),
+        y: Math.round(element.offsetTop + element.offsetHeight / 2 - tooltip.offsetHeight / 2),
       };
       break;
     }
-    case 'bottom': {
-      transform = { 
-        x: tooltip.offsetWidth > element.offsetWidth ? Math.round(element.offsetLeft - (Math.abs(element.offsetWidth - tooltip.offsetWidth)/2)) : Math.round(element.offsetLeft + (Math.abs(element.offsetWidth - tooltip.offsetWidth)/2)),
-        y: Math.round(element.offsetTop + element.offsetHeight + 10)
+    case "bottom": {
+      transform = {
+        x:
+          tooltip.offsetWidth > element.offsetWidth
+            ? Math.round(element.offsetLeft - Math.abs(element.offsetWidth - tooltip.offsetWidth) / 2)
+            : Math.round(element.offsetLeft + Math.abs(element.offsetWidth - tooltip.offsetWidth) / 2),
+        y: Math.round(element.offsetTop + element.offsetHeight + 10),
       };
       break;
     }
-    case 'right': {
-      transform = { 
-        x: Math.round(element.offsetLeft + element.offsetWidth + 10), 
-        y: Math.round(element.offsetTop + (element.offsetHeight / 2) - (tooltip.offsetHeight / 2))
+    case "right": {
+      transform = {
+        x: Math.round(element.offsetLeft + element.offsetWidth + 10),
+        y: Math.round(element.offsetTop + element.offsetHeight / 2 - tooltip.offsetHeight / 2),
       };
       break;
     }
     default: {
-      transform = { 
-        x: tooltip.offsetWidth > element.offsetWidth ? Math.round(element.offsetLeft - (Math.abs(element.offsetWidth - tooltip.offsetWidth)/2)) : Math.round(element.offsetLeft + (Math.abs(element.offsetWidth - tooltip.offsetWidth)/2)),
-        y: Math.round(element.offsetTop - tooltip.offsetHeight - 10)
+      transform = {
+        x:
+          tooltip.offsetWidth > element.offsetWidth
+            ? Math.round(element.offsetLeft - Math.abs(element.offsetWidth - tooltip.offsetWidth) / 2)
+            : Math.round(element.offsetLeft + Math.abs(element.offsetWidth - tooltip.offsetWidth) / 2),
+        y: Math.round(element.offsetTop - tooltip.offsetHeight - 10),
       };
       break;
     }
   }
   return `translate3d(${transform.x}px, ${transform.y}px, 0px)`;
-}
+};
 
-const createTooltip = function(element) {
-  let tooltipElement = document.createElement('div');
-  tooltipElement.setAttribute('class', 'tooltip');
-  tooltipElement.setAttribute('id', 'tooltip-kutty');
-  tooltipElement.innerText = element.getAttribute('title');
-  tooltipElement.style.transformOrigin = 'center center';
-  tooltipElement.style.willChange = 'transform';
+const createTooltip = function (element) {
+  let tooltipElement = document.createElement("div");
+  tooltipElement.setAttribute("class", "tooltip");
+  tooltipElement.setAttribute("role", "tooltip");
+  tooltipElement.setAttribute("id", "tooltip-kutty");
+  tooltipElement.innerText = element.getAttribute("title");
   document.body.append(tooltipElement);
   tooltipElement.style.transform = getTooltipPlacement(element, tooltipElement);
-}
+};
 
-const destroyTooltip = function(element) {
-  let tooltipElement = document.getElementById('tooltip-kutty');
+const destroyTooltip = function (element) {
+  let tooltipElement = document.getElementById("tooltip-kutty");
   return tooltipElement.parentNode.removeChild(tooltipElement);
-}
+};
 
 window.tooltip = function () {
   return {
@@ -66,6 +74,6 @@ window.tooltip = function () {
       ["@mouseleave"]() {
         destroyTooltip(this.$el);
       },
-    }
+    },
   };
 };
