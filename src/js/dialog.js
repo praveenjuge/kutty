6e86127c96548d3475ad7fe56d84be01173622f7
TheWebDevel <sathish@skcript.com>
2020-08-16 17:22:03 +0530

    completed dialog with alpine js

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -7,20 +7,71 @@
 // dialog modal
 // - Press escape or click outside the dialog modal to close the dialog modal
 // - Set aria-haspopup="dialog" attribute to dialog trigger
-// - Toggle aria-hidden attribute to dialog modal based on it's open state
 // - Add role="dialog" attribute to dialog modal
 // - Add aria-modal="true" attribute to dialog modal
 // - Add tabindex="-1" to dialog modal
 // - Restore focus to the dialog trigger button once the dialog modal is closed
 // - Make sure it's closable with a button inside the dialog modal
+// - Toggle aria-hidden attribute to dialog modal based on it's open state
 // When the dialog modal is opened, do the following:
 // - Focus on the first focusable element when dialog modal is opened
 // - Make sure tab focuses are restricted to modal itself
-// - Add overflow: hidden; style to html
-// - Add padding-right style to the body, where the value is the scroll bar
-// width
+// - Add overflow: hidden; style to html tag
+// - Add padding-right style to the body, where the value is the scroll bar -
+// find the scroll bar width and set as padding right to html body width
 
+// Data
 var lastOpenedElement = null;
+const FOCUSABLE_ELEMENTS = [
+  '[href]:not([tabindex^="-"])',
+  'input:not([disabled]):not([type="hidden"]):not([tabindex^="-"]):not([type="radio"])',
+  'input[type="radio"]:checked',
+  'select:not([disabled]):not([tabindex^="-"])',
+  'textarea:not([disabled]):not([tabindex^="-"])',
+  'button:not([disabled]):not([tabindex^="-"])',
+  '[tabindex]:not([tabindex^="-"])',
+  '[contenteditable="true"]:not([tabindex^="-"])',
+];
+
+// Set attributes when the component is initialized
+function init(dialog) {
+  var trigger = dialog.querySelectorAll('[x-spread="trigger"]');
+  if (trigger.length) {
+    trigger = trigger[0];
+    trigger.setAttribute('aria-haspopup', 'dialog');
+    var dialog = dialog.querySelectorAll('[x-spread="dialog"]');
+    if (dialog.length) {
+      dialog = dialog[0];
+      dialog.setAttribute('role', 'dialog');
+      dialog.setAttribute('aria-hidden', true);
+      dialog.setAttribute('aria-modal', true);
+      dialog.setAttribute('tabindex', -1);
+    }
+  }
+}
+
+// Toggle aria atrributes based on the dialog state
+function toggleAriaAtrributes(dialog, isOpen) {
+  dialog = dialog.querySelectorAll('[x-spread="dialog"]');
+  if (dialog.length) {
+    dialog = dialog[0];
+    if (isOpen) {
+      dialog.setAttribute('aria-hidden', false);
+    } else {
+      dialog.setAttribute('aria-hidden', true);
+    }
+  }
+}
+
+// Focus an element
+// elements - array of dropdown elements
+function focus(elements, focussedIndex) {
+  // Use of % for infinite iterations
+  var index = Math.abs(focussedIndex) % elements.length;
+  if (index >= 0) {
+    elements[index].focus();
+  }
+}
 
 // Refocus dialog trigger element
 // This usually happens when the user closes the dialog
@@ -31,7 +82,48 @@ function refocus(element) {
   }
 }
 
+// Focus Dialog
+function focusDialog(dialog) {
+  dialog = dialog.querySelector('[x-spread="dialog"]');
+  if (dialog) {
+    dialog.focus()
+  }
+}
+
+// From bootstrap
+function getScrollbarWidth() {
+  const scrollDiv = document.createElement('div');
+  scrollDiv.className = 'scrollbar-measure';
+  document.body.appendChild(scrollDiv);
+  const scrollbarWidth =
+      scrollDiv.getBoundingClientRect().width - scrollDiv.clientWidth;
+  document.body.removeChild(scrollDiv);
+  return scrollbarWidth;
+}
+
+function adjustScrollBar(isOpen) {
+  if (isOpen) {
+    var htmlDocument = document.querySelector('html');
+    htmlDocument.style.overflow = 'hidden';
+    var body = document.querySelector('body');
+    body.style.paddingRight = getScrollbarWidth() + 'px';
+  } else {
+    var htmlDocument = document.querySelector('html');
+    htmlDocument.style.overflow = null;
+    var body = document.querySelector('body');
+    body.style.paddingRight = null;
+  }
+}
+
+
+// Initialize attribute for all dialog elements
+var dialogs = document.querySelectorAll('[x-data="dialog()"]');
+dialogs.forEach(function(dialog) {
+  init(dialog);
+});
+
 window.dialog = function() {
+  var focussedIndex = -1;
   return {
     open: false,
     trigger: {
@@ -39,23 +131,47 @@ window.dialog = function() {
         this.open = !this.open;
         if (this.open) {
           lastOpenedElement = this.$el;
+          focusDialog(this.$el);
+        } else {
+          focussedIndex = -1;
         }
+        adjustScrollBar(this.open);
+        toggleAriaAtrributes(this.$el, this.open);
       },
       ['@keydown.escape']() {
         this.open = false;
+        focussedIndex = -1;
         refocus(lastOpenedElement);
+        adjustScrollBar(this.open);
+        toggleAriaAtrributes(this.$el, this.open);
       },
+      ['@keydown.tab'](e) {
+        if (this.open) {
+          e.preventDefault();
+          var dialog = document.querySelector('[x-spread="dialog"]');
+          var dialogElements = dialog.querySelectorAll(FOCUSABLE_ELEMENTS);
+          focussedIndex++;
+          focus(dialogElements, focussedIndex);
+        }
+      }
     },
     dialog: {
-      close() {
-        this.open = false
-      },
       ['x-show.transition.opacity.duration.200ms']() {
         return this.open;
       },
       ['@keydown.escape']() {
         this.open = false;
+        focussedIndex = -1;
         refocus(lastOpenedElement);
+        adjustScrollBar(this.open);
+        toggleAriaAtrributes(this.$el, this.open);
+      },
+      ['@keydown.tab'](e) {
+        e.preventDefault();
+        var dialog = document.querySelector('[x-spread="dialog"]');
+        var dialogElements = dialog.querySelectorAll(FOCUSABLE_ELEMENTS);
+        focussedIndex++;
+        focus(dialogElements, focussedIndex);
       },
     },
   };
