d2d6632bce038298fde422e5ced760a6c654a87d
praveenjuge <praveen@skcript.com>
2020-09-06 22:11:23 +0530

    Added Blog Post Components
    Signed-off-by: praveenjuge <praveen@skcript.com>

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,30 +1,6 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 module.exports = {
   purge: {
     content: ["./docs/layouts/**/*.html", "./docs/content/**/*.md", "./docs/content/**/*.html", "./src/js/**/*.js"],
-  },
-  theme: {
-    typography: {
-      default: {
-        css: {
-          lineHeight: "inherit",
-          h1: {
-            fontWeight: "700",
-          },
-          h2: {
-            marginBottom: defaultTheme.spacing[3],
-          },
-          pre: {
-            color: defaultTheme.colors.gray[900],
-            backgroundColor: defaultTheme.colors.gray[200] + "!important",
-            lineHeight: "inherit",
-            marginBottom: 0,
-            marginTop: 0,
-          },
-        },
-      },
-    },
   },
   variants: {
     visibility: ["group-hover"],
