32f12d6005fd6707e305967ca9b093f057d2e3fb
praveenjuge <praveen@skcript.com>
2020-10-18 11:18:56 +0530

    Alert uses utility classes for color now
    Signed-off-by: praveenjuge <praveen@skcript.com>

--- docs/content/docs/typography.md
+++ docs/content/docs/typography.md
@@ -4,7 +4,7 @@ title: Typography
 
 ## Basic
 
-<div class="alert alert-primary my-6" role="alert">
+<div class="alert bg-primary-light text-primary my-6" role="alert">
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="16" x2="12" y2="12"></line><line x1="12" y1="8" x2="12.01" y2="8"></line></svg>
   <p>Kutty uses <a href="https://github.com/tailwindlabs/tailwindcss-typography" target="_blank" rel="noopener noreferrer">Tailwind's Typography Plugin</a>, check out their <a href="https://github.com/tailwindlabs/tailwindcss-typography" target="_blank" rel="noopener noreferrer">documentation</a> for more information.</p>
 </div>
--- src/alert.js
+++ src/alert.js
@@ -1,10 +1,10 @@
 module.exports = Alert = () => ({
   ".alert": {
-    "@apply font-medium relative overflow-hidden border-2 border-solid text-sm rounded-md align-middle duration-150 ease-in flex items-start py-2 pl-4 pr-4": {},
+    "@apply font-medium relative overflow-hidden text-sm rounded-md align-middle duration-150 ease-in flex items-start pl-4 pr-4 border-current": {},
+    paddingTop: '0.65rem',
+    paddingBottom: '0.65rem',
   },
-  ".alert a": {
-    "@apply underline": {},
-  },
+  // Close
   ".alert-close": {
     "@apply absolute top-0 bottom-0 right-0 !important": {},
   },
@@ -17,83 +17,22 @@ module.exports = Alert = () => ({
   ".alert svg": {
     "@apply h-5 w-5 mr-3 mt-px flex-none": {},
   },
+  // Sizes
+  ".alert-sm": {
+    "@apply py-1 pl-3 pr-3 text-xs !important": {},
+  },
+  ".alert-lg": {
+    "@apply py-5 pl-5 pr-5 text-lg !important": {},
+  },
   ".alert.alert-sm svg": {
     "@apply h-4 w-4": {},
   },
   ".alert.alert-lg svg": {
     "@apply h-6 w-6": {},
   },
-  ".alert-sm": {
-    "@apply py-1 pl-2 pr-2 text-xs !important": {},
-  },
-  ".alert-lg": {
-    "@apply py-5 pl-5 pr-5 text-lg !important": {},
-  },
-  ".alert-description": {
-    "@apply flex text-sm font-normal": {},
-  },
-  ".alert-primary": {
-    "@apply bg-primary-light border-primary-light text-primary": {},
-  },
-  ".alert-success": {
-    "@apply bg-green-200 border-green-200 text-green-800": {},
-  },
-  ".alert-danger": {
-    "@apply bg-red-200 border-red-200 text-red-700": {},
-  },
-  ".alert-warning": {
-    "@apply bg-yellow-200 border-yellow-200 text-yellow-800": {},
-  },
-  ".alert-light": {
-    "@apply bg-gray-200 border-gray-200 text-gray-900": {},
-  },
-  ".alert-dark": {
-    "@apply bg-gray-900 border-gray-900 text-white": {},
-  },
-  ".alert-outline-primary": {
-    "@apply border-current text-primary bg-transparent": {},
-  },
-  ".alert-outline-secondary": {
-    "@apply border-current text-gray-700 bg-transparent": {},
-  },
-  ".alert-outline-success": {
-    "@apply border-current text-green-700 bg-transparent": {},
-  },
-  ".alert-outline-danger": {
-    "@apply border-current text-red-700 bg-transparent": {},
-  },
-  ".alert-outline-warning": {
-    "@apply border-current text-yellow-700 bg-transparent": {},
-  },
-  ".alert-outline-light": {
-    "@apply border-current text-gray-200 bg-transparent": {},
-  },
-  ".alert-outline-dark": {
-    "@apply border-current text-gray-900 bg-transparent": {},
-  },
-  ".alert-solid-primary": {
-    "@apply bg-primary border-primary text-white": {},
-  },
-  ".alert-solid-success": {
-    "@apply bg-green-700 border-green-700 text-white": {},
-  },
-  ".alert-solid-danger": {
-    "@apply bg-red-700 border-red-700 text-white": {},
-  },
-  ".alert-solid-warning": {
-    "@apply bg-yellow-600 border-yellow-600 text-white": {},
-  },
-  ".alert-top": {
-    "@apply border border-solid border-t-4 border-r-0 border-b-0 border-l-0 border-current rounded-none !important": {},
-  },
-  ".alert-left": {
-    "@apply border border-solid border-l-4 border-r-0 border-b-0 border-t-0 border-current rounded-none !important": {},
-  },
-  ".alert-right": {
-    "@apply border border-solid border-r-4 border-l-0 border-b-0 border-t-0 border-current rounded-none !important": {},
-  },
-  ".alert-bottom": {
-    "@apply border border-solid border-b-4 border-l-0 border-r-0 border-t-0 border-current rounded-none !important": {},
+  // Text
+  ".alert a": {
+    "@apply underline": {},
   },
   ".prose .alert p": {
     "@apply mt-0 mb-0": {},
